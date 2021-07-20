31cacfd807a82b69d8bce8f8ff621dfa66d1674c
MurilloSSJ <murillostore@gmail.com>
2021-07-19 23:16:26 -0300

    Configurando database

--- api/app.js
+++ api/app.js
@@ -1,8 +1,23 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
+const db = require('./dataBase/db')
 
 //Configurações 
     //bodyParser
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
+    //DataBase
+    mongoose.connect(db.mongoURI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log("Conectado ao DB")
+    })
+
+
+app.listen(PORT,()=>{
+    console.log('Servidor ativo')
+})
---
+++ api/dataBase/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://teste-estagio:testeestagio1234@teste-estagio-loglife.4u7dl.mongodb.net/teste-estagio-loglife?retryWrites=true&w=majority'
+}
