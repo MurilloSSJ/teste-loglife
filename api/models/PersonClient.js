b8ed076ba60e9fd9f170e95c31c14cf331f23065
MurilloSSJ <murillostore@gmail.com>
2021-07-20 00:04:50 -0300

    Criação do model PersonClient

---
+++ api/models/PersonClient.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const PersonClient = new Schema({
+    situation:{
+        type:Boolean,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    cpf:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    cep:{
+        type:String,
+        required:true
+    },
+    street:{
+        type:String,
+        required:true
+    },
+    adressNumber:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    openingHours:{
+        type: Date,
+        required:true
+    },
+    attendanceDay:{
+        type:Date,
+        required:true
+    },
+    vehicles:[{
+        type:String,
+        required:true
+    }]
+})
