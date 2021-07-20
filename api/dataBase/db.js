49323b1676e93d3ced3803d9204dc7a85927024a
MurilloSSJ <murillostore@gmail.com>
2021-07-20 10:31:59 -0300

    Terminando as rotas

--- api/dataBase/db.js
+++ api/dataBase/db.js
@@ -1,3 +1,6 @@
-module.exports = {
-    mongoURI : 'mongodb+srv://teste-estagio:testeestagio1234@teste-estagio-loglife.4u7dl.mongodb.net/teste-estagio-loglife?retryWrites=true&w=majority'
-}
+    if(process.env.NODE_ENV=='production'){
+        module.exports = { mongoURI : 'mongodb+srv://teste-estagio:testeestagio1234@teste-estagio-loglife.4u7dl.mongodb.net/teste-estagio-loglife?retryWrites=true&w=majority'}
+    }
+    else{
+       module.exports = {mongoURI:'mongodb://localhost/teste-estagio-loglife'}
+    }
--- api/routes/clients.js
+++ api/routes/clients.js
@@ -7,14 +7,44 @@ const personClient = mongoose.model('Cliente')
 const enterpriseClient = mongoose.model('Cliente_Juridico')
 
 //Criando as rotas da API
+//Obter todos os clients
 router.get('/search',(req,res)=>{
-    personClient.find().then((personsClients)=>{
-        enterpriseClient.find().then((enterprisesClients)=>{
+    personClient.find().lean().then((personsClients)=>{
+        enterpriseClient.find().lean().then((enterprisesClients)=>{
             res.send({personClient:personsClients,enterpriseClient:enterprisesClients})
         })
     })
     
 })
+//Obter os clients por tipo
+router.get('/search/:typeClient',(req,res)=>{
+    const typeClient = req.params.typeClient
+    if(typeClient == 'clientEnterprise'){
+        enterpriseClient.find().lean().then((clients)=>{
+            res.send({enterpriseClients:clients})
+        })
+    }else if(typeClient == 'clientPerson'){
+        personClient.find().then((clients)=>{
+            res.send({personClients:clients})
+        })
+    }
+})
+//Obter apenas um client
+router.get('/search/:typeClient/:id',(req,res)=>{
+    const typeClient = req.params.typeClient
+    if(typeClient == 'clientEnterprise'){
+        enterpriseClient.findOne({_id:req.params.id}).lean().then((client)=>{
+            res.send({enterpriseClient:client})
+        })
+    }else if(typeClient == 'clientPerson'){
+        personClient.findOne({_id:req.params.id}).lean().then((client)=>{
+            res.send({personClient:client})
+        })
+    }
+    
+})
+
+//Adicionar novo client
 router.post('/add',(req,res)=>{
     if(req.body.typeClient=='clientPerson'){
         const newClient = {
@@ -55,6 +85,72 @@ router.post('/add',(req,res)=>{
         }
         new enterpriseClient(newClient).save().then(()=>{
             console.log('salvo com sucesso')
+        })
+    }
+})
+router.delete('/delete/:typeClient/:id',(req,res)=>{
+    const typeClient = req.params.typeClient
+    const id = req.params.id
+    if(typeClient == 'clientEnterprise'){
+        enterpriseClient.remove({_id:id}).then(()=>{
+            console.log('Removido com sucesso')
+        })
+        
+    }else if(typeClient == 'clientPerson'){
+        personClient.remove({_id:id}).then(()=>{
+            console.log('success')
+            res.redirect('http://localhost:5000/clients/search')
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+})
+
+router.put('/alter/:typeClient/:id',(req,res)=>{
+    const typeClient = req.params.typeClient
+    const id = req.params.id
+    if(typeClient == 'clientEnterprise'){
+        enterpriseClient.findOne({_id:id}).then((editClient)=>{
+            editClient.situation=req.body.situation,
+            editClient.name=req.body.name,
+            editClient.socialReason=req.body.lastName,
+            editClient.cnpj=req.body.cpfcnpj,
+            editClient.email=req.body.email,
+            editClient.phone=req.body.phone,
+            editClient.cep=req.body.cep,
+            editClient.street=req.body.street,
+            editClient.adressNumber=req.body.adress,
+            editClient.city=req.body.city,
+            editClient.country=req.body.country,
+            editClient.openingHours=req.body.opening,
+            editClient.attendanceDay=req.body.attendance,
+            editClient.vehicles=req.body.vehicles
+            editClient.save().then(()=>{
+                console.log('success')
+                res.redirect('http://localhost:5000/clients/search')
+            })
+        })
+        
+    }else if(typeClient == 'clientPerson'){
+        personClient.findOne({_id:id}).then((editClient)=>{
+            editClient.situation=req.body.situation,
+            editClient.name=req.body.name,
+            editClient.lastName=req.body.lastName,
+            editClient.cpf=req.body.cpfcnpj,
+            editClient.email=req.body.email,
+            editClient.phone=req.body.phone,
+            editClient.cep=req.body.cep,
+            editClient.street=req.body.street,
+            editClient.adressNumber=req.body.adress,
+            editClient.city=req.body.city,
+            editClient.country=req.body.country,
+            editClient.openingHours=req.body.opening,
+            editClient.attendanceDay=req.body.attendance,
+            editClient.vehicles=req.body.vehicles
+            editClient.save().then(()=>{
+                console.log('success')
+                res.redirect('http://localhost:5000/clients/search')
+            })
         })
     }
 })
